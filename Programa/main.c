5362b30e67b9d6e7d6e2792296f1700fc933f3be
Grgcrz <50218524+Grgcrz@users.noreply.github.com>
2019-06-25 17:53:23 -0400

    cabeza de la factura listo

--- Programa/main.c
+++ Programa/main.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "funciones.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    //Esta estructura es un ejemplo para corregir la funcion imprime_titulo();
+    struct client
+    {
+        char nom[20];
+        char app[20];
+        char dir[100];
+        char tel[200];
+    }cliente = {
+    "PUCMM",
+    "Exportaciones CxA",
+    "Autopista Duarte,km 7,Santiago de los Caballeros,Rep.Dom",
+    "Tel: 809-824-0210,Email:expoPUCMM@gmail.com"
+    };
+    //Aqui termina;
+    imprime_titulo(cliente.nom,cliente.app,cliente.dir,cliente.tel,3);
     return 0;
 }
