be00a6b82a49359102d6879353aac20854aabe31
Grgcrz <50218524+Grgcrz@users.noreply.github.com>
2019-06-30 23:22:01 -0400

    30\6\19 - ya se imprme en txt

--- Programa/funciones.h
+++ Programa/funciones.h
@@ -1,59 +1,78 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
-void imprime_salir(){
-getch();
-printf("\n\nDesea salir?\n");
-printf("0 - si\n1 - regresar al inicio\n");
+
+void imprime_salir()
+{
+    getch();
+    printf("\n\nDesea salir?\n");
+    printf("0 - si\n1 - regresar al inicio\n");
 }
-void imprime_menu(){
+void imprime_menu()
+{
     system("color 8f");
-for (int n = 0; n < 25; n++){
-printf("%c",176);
+    for (int n = 0; n < 25; n++)
+    {
+        printf("%c",176);
+    }
+    printf("\tFacturadora 1.0.0\t");
+    for (int k = 0; k < 25; k++)
+    {
+        printf("%c",176);
+    }
+    puts("\n\n");
+    printf("\t%s\t\n\t%s\t\n\t%s\n\t%s\n\t%s\n","1 - Clientes y datos de envio","2 - Productos y unidades","3 - Generar factura","4 - Archivo de facturas","5 - Ventas del mes");
+    puts("\n\n\tIngrese el numero con la funcion deseada: ");
 }
-printf("\tFacturadora 1.0.0\t");
-for (int k = 0; k < 25; k++){
-printf("%c",176);
-}
-puts("\n\n");
-printf("\t%s\t\n\t%s\t\n\t%s\n\t%s\n","1 - Clientes y datos de envio","2 - Productos y unidades","3 - Generar factura","4 - Archivo de facturas");
-puts("\n\n\tIngrese el numero con la funcion deseada: ");
-}
-void imprime_titulo(char name[],char nomber[],char direccion[],char telefono[],int ID[],int n){
+void imprime_titulo(char name[],char nomber[],char direccion[],char telefono[],int ID[],int n)
+{
     system("color f0");
-    for(int i = 0; i < 20; i++){
-    printf("%c",177);}
+    for(int i = 0; i < 20; i++)
+    {
+        printf("%c",177);
+    }
     printf("\t%s %s \t","PUCMM EXPORTACIONES","CXA");
-    for(int i = 0; i < 20; i++){
-    printf("%c",177);}
+    for(int i = 0; i < 20; i++)
+    {
+        printf("%c",177);
+    }
     printf("\n\t%s"," Edificios 'El matadero', Autopista Duarte Km 1 1/2 Santiago, RD.");
     printf("\n\t %s\t%s","Tel: 809-201-8036","Email:ventasMATADERO@pucmm.edu.do");
     printf(" \n\n\t\t\t%s\t","Factura comercial final");
     puts("\n");//barra de abajo
     printf("%c",218);//-|
-    for(int k = 0; k < 75; k++){
+    for(int k = 0; k < 75; k++)
+    {
         printf("%c",196);
     }
     printf("%c\n%cCliente:%s",191,179,name);
-    printf("%\tNo.Factura:%s","12345");
+    printf("\tNo.Factura:%s","12345");
     printf("\t Fecha:%s\t    %c","33434343",179);
     //printf("%c",179);
     printf("\n%cDirecci%cn:%s",179,162,direccion);
     printf("\t    %c\n%cTel:%s\tEmail:%s\tID/RNC:%s",179,179,telefono,"ejemploemail@ejemplo.com",ID);
     printf("  %c\n%c",179,192);
-    for(int k = 0; k < 75; k++){
+    for(int k = 0; k < 75; k++)
+    {
         printf("%c",196);
-        }
+    }
     printf("%c",217);
     printf("\n");
 }
-void imprime_cabecera_del_cuerpo(){
-puts("\n");//barra de abajo
+void imprime_cabecera_del_cuerpo()
+{
+    puts("\n");//barra de abajo
     printf("%c",218);//-|
-    for(int k = 0; k < 75; k++){
-        if (k == 7 || k == 20 || k == 55 || k == 64 ){
-        printf("%c",194);}else {
-    printf("%c",196);}
+    for(int k = 0; k < 75; k++)
+    {
+        if (k == 7 || k == 20 || k == 55 || k == 64 )
+        {
+            printf("%c",194);
+        }
+        else
+        {
+            printf("%c",196);
+        }
     }
     printf("%c",191);
     printf("\n%c   ID\t",179);
@@ -62,27 +81,41 @@ puts("\n");//barra de abajo
     printf("%c Precio ",179);
     printf("%c   Peso   %c",179,179);
     printf("\n%c",192);
-    for(int k = 0; k < 75; k++){
-        if (k == 7 || k == 20 || k == 55 || k == 64 ){
-        printf("%c",193);}else {
-    printf("%c",196);}
+    for(int k = 0; k < 75; k++)
+    {
+        if (k == 7 || k == 20 || k == 55 || k == 64 )
+        {
+            printf("%c",193);
+        }
+        else
+        {
+            printf("%c",196);
+        }
     }
     printf("%c",217);//-|
 }
-
-void seccion_productos_y_peso(){
-system("cls");
-system("color f0");
-printf("\t\t\t\t\Productos y peso\t\t\t\t");
+void seccion_productos_y_peso()
+{
+    system("cls");
+    system("color f0");
+    printf("\t\t\t\t\Productos y peso\t\t\t\t");
 }
-void seccion_Archivo_factura(){
-system("cls");
-system("color f0");
-printf("\t\t\t\t\Productos y peso\t\t\t\t");
+void seccion_Archivo_factura()
+{
+    system("cls");
+    system("color f0");
+    printf("\t\t\t\t\Productos y peso\t\t\t\t");
 }
-void seccion_Clientes(){
-system("cls");
-system("color 0f");
-printf("\t\t\t\t\Clientes y direcci%cn\t\t\t\t",161);
+void seccion_Clientes()
+{
+    system("cls");
+    system("color 0f");
+    printf("\t\t\t\t\Clientes y direcci%cn\t\t\t\t",161);
+}
+void seccion_ventas_mes()
+{
+    system("cls");
+    system("color 0f");
+    printf("\t\t\t\t\t\tVentas del mes\t\t\t\t\n\n",161);
 }
 //system("f0") fondo blanco, letras negras. 9, 22, 57,67.
--- Programa/main.c
+++ Programa/main.c
@@ -24,6 +24,82 @@ case 3:
     system("cls");
     imprime_titulo(cliente.nom,cliente.app,cliente.dir,cliente.tel,cliente.ID,3);
     imprime_cabecera_del_cuerpo();
+    //Imprime la factura en txt
+   FILE *print;
+   print = fopen("factura_0.txt","w");
+   if ((print = fopen("factura_0.txt","w")) != NULL)
+   {
+       for(int i = 0; i < 20; i++)
+    {
+        fprintf(print,"#");
+    }
+    fprintf(print,"\t%s %s \t","PUCMM EXPORTACIONES","CXA");
+    for(int i = 0; i < 20; i++)
+    {
+        fprintf(print,"#");
+    }
+    fprintf(print,"\n\t%s"," Edificios 'El matadero', Autopista Duarte Km 1 1/2 Santiago, RD.");
+    fprintf(print,"\n\t %s\t%s","Tel: 809-201-8036","Email:ventasMATADERO@pucmm.edu.do");
+    fprintf(print," \n\n\t\t\t%s\t","Factura comercial final");
+    fprintf(print,"\n");//barra de abajo
+    fprintf(print,"%c",'+');
+    for(int k = 0; k < 75; k++)
+    {
+        fprintf(print,"%c",'-');
+    }
+    fprintf(print,"%c\n%cCliente:%s",'+','|',cliente.nom);
+    fprintf(print,"%\tNo.Factura:%s","12345");
+    fprintf(print,"\t Fecha:%s\t    %c","33434343",'|');
+    //printf("%c",179);
+    fprintf(print,"\n%cDirecci%cn:%s",'|','o',cliente.dir);
+    fprintf(print,"\t    %c\n%cTel:%s\tEmail:%s\tID/RNC:%s",'|','|',cliente.tel,"ejemploemail@ejemplo.com",cliente.ID);
+    fprintf(print,"  %c\n%c",'|','+');
+    for(int k = 0; k < 75; k++)
+    {
+        fprintf(print,"%c",'-');
+    }
+    fprintf(print,"%c",'+');
+    fprintf(print,"\n");
+
+// Encabezado descripcion de la caja de productos
+fprintf(print,"\n");//barra de abajo
+    fprintf(print,"%c",'+');//-|
+    for(int k = 0; k < 75; k++)
+    {
+        if (k == 7 || k == 20 || k == 55 || k == 64 )
+        {
+            fprintf(print,"%c",'+');
+        }
+        else
+        {
+            fprintf(print,"%c",'-');
+        }
+    }
+    fprintf(print,"%c",'+');
+    fprintf(print,"\n%c   ID\t",'|');
+    fprintf(print,"%c  Cantidad  ",'|');
+    fprintf(print,"%c\t\tProducto\t\t",'|');
+    fprintf(print,"%c Precio ",'|');
+    fprintf(print,"%c   Peso   %c",'|','|');
+    fprintf(print,"\n%c",'+');
+    for(int k = 0; k < 75; k++)
+    {
+        if (k == 7 || k == 20 || k == 55 || k == 64 )
+        {
+            fprintf(print,"%c",'+');
+        }
+        else
+        {
+            fprintf(print,"%c",'-');
+        }
+    }
+    fprintf(print,"%c",'+');
+
+       printf("\n\nLa factura fue generada correctamente!");
+   } else{
+    printf("ERROR: La factura no se ha generado.");
+   }
+   fclose(print);
     imprime_salir();
     scanf("%d",&end);
     system("cls");
@@ -33,7 +109,16 @@ case 3:
     seccion_Archivo_factura();
     scanf("%d",&end);
     system("cls");
+    break;
+case 5:
+    seccion_ventas_mes();
+    imprime_salir();
+    scanf("%d",&end);
+    system("cls");
     break;}
 } while (end != 0);
   return 0;
 }
+
+
+
